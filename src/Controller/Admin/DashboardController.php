1d77d2e2063895914f2a817fd94819bb02e7c49c
Frederic Branca <frederic.branca@gmail.com>
2023-08-22 17:01:00 +0200

    Configuration d'easyadmin (j'affiche toute la db)

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,34 +2,28 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\User;
+use App\Entity\Module;
+use App\Entity\Session;
+use App\Entity\Categorie;
+use App\Entity\Formation;
+use App\Entity\Programme;
+use App\Entity\Stagiaire;
+use App\Controller\SessionController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(SessionCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
@@ -40,7 +34,12 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Session', 'fas fa-calendar', Session::class);
+        yield MenuItem::linkToCrud('Formation', 'fas fa-graduation-cap', Formation::class);
+        yield MenuItem::linkToCrud('Module', 'fas fa-cubes', Module::class);
+        yield MenuItem::linkToCrud('Catégorie', 'fas fa-tasks', Categorie::class);
+        yield MenuItem::linkToCrud('Programme', 'fas fa-square', Programme::class);
+        yield MenuItem::linkToCrud('Stagiaire', 'fas fa-address-book', Stagiaire::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-user', User::class);
     }
 }
