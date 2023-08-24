2c1721040a6b760ee96f59218427160259f609b5
Frederic Branca <frederic.branca@gmail.com>
2023-08-24 16:50:09 +0200

    Route pour afficher les sessions de formation

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Session;
+use App\Repository\SessionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
+    // Route pour afficher la liste des sessions
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    public function index(SessionRepository $sessionRepository): Response
     {
+        $sessionsEnCours = $sessionRepository->sessionsEnCours();
+        $sessionsTerminee = $sessionRepository->sessionsTerminee();
+        $sessionsFuture = $sessionRepository->sessionsFuture();
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'enCours' => $sessionsEnCours,
+            'terminees' => $sessionsTerminee,
+            'future' => $sessionsFuture
         ]);
     }
 }
