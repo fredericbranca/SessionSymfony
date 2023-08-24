aaf20db8ec9480d98cf40012e8145df458d01ffc
Frederic Branca <frederic.branca@gmail.com>
2023-08-24 11:14:28 +0200

    Ajout de l 'autocomplete off sur le champ nom cat

--- src/Form/CategorieType.php
+++ src/Form/CategorieType.php
@@ -6,6 +6,7 @@ use App\Entity\Categorie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CategorieType extends AbstractType
@@ -13,8 +14,12 @@ class CategorieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('Ajouter', SubmitType::class)
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'autocomplete' => 'off'
+                ]
+            ])
+            ->add('Valider', SubmitType::class)
         ;
     }
 
