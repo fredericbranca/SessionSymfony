f49b772f4e66c64390bd2e12e05f3f54574dce13
Frederic Branca <frederic.branca@gmail.com>
2023-08-25 16:53:45 +0200

    En cours : supprimer le programme d'une session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,15 +3,30 @@
 namespace App\Controller;
 
 use App\Entity\Session;
+use App\Entity\Programme;
 use App\Repository\SessionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class SessionController extends AbstractController
 {
+    // Route pour supprimer un programme d'une session
+    #[Route('/session/{id}/deleteProgramme/{idProgramme}', name: 'deleteProgramme_session')]
+    public function deleteProgramme(
+    #[MapEntity(id: 'id')] Session $session, #[MapEntity(id: 'idProgramme')] Programme $programme, EntityManagerInterface $entityManager) : Response
+    {
+        $session->removeProgramme($programme);
+        $entityManager->persist($session);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('infos_session', ['id' => $session->getId()]);
+    }
+
     // Route pour afficher la liste un ou plusieurs sessions
     #[Route('/session', name: 'app_session')]
     #[Route('/session/{id}', name: 'infos_session')]
