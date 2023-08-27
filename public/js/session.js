d9410ce50fe0f9bb7104d40a846d85886b1b78c8
Frederic Branca <frederic.branca@gmail.com>
2023-08-27 13:23:33 +0200

    Script pour rediriger les lignes du table au clic

---
+++ public/js/session.js
@@ -0,0 +1,6 @@
+// Rendre cliquable les lignes d'un tableau et rediriger vers l'URL spécifiée
+document.querySelectorAll('.clickable-row').forEach(function(row) {
+    row.addEventListener('click', function() {
+        window.location.href = row.getAttribute('data-url');
+    });
+});
