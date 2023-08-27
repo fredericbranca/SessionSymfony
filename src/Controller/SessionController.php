888ffe4c52c19139b2ca999e9f02fd0395016b1a
Frederic Branca <frederic.branca@gmail.com>
2023-08-27 13:32:08 +0200

    route pour supprimer le stagiaire d'une session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Session;
 use App\Entity\Programme;
+use App\Entity\Stagiaire;
 use App\Repository\SessionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,9 +19,25 @@ class SessionController extends AbstractController
     // Route pour supprimer un programme d'une session
     #[Route('/session/{id}/deleteProgramme/{idProgramme}', name: 'deleteProgramme_session')]
     public function deleteProgramme(
-    #[MapEntity(id: 'id')] Session $session, #[MapEntity(id: 'idProgramme')] Programme $programme, EntityManagerInterface $entityManager) : Response
-    {
+        #[MapEntity(id: 'id')] Session $session,
+        #[MapEntity(id: 'idProgramme')] Programme $programme,
+        EntityManagerInterface $entityManager
+    ): Response {
         $entityManager->remove($programme);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('infos_session', ['id' => $session->getId()]);
+    }
+
+    // Route pour supprimer un stagiaire d'une session
+    #[Route('/session/{id}/deleteStagiaire/{idStagiaire}', name: 'deleteStagiaire_session')]
+    public function deleteStagiaire(
+        #[MapEntity(id: 'id')] Session $session,
+        #[MapEntity(id: 'idStagiaire')] Stagiaire $stagiaire,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $session->removeStagiaire($stagiaire);
+        $entityManager->persist($session);
         $entityManager->flush();
 
         return $this->redirectToRoute('infos_session', ['id' => $session->getId()]);
