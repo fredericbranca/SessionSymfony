ff0650c352a7d4bc6cb0734f39d2281fddc09140
Frederic Branca <frederic.branca@gmail.com>
2023-08-28 16:54:37 +0200

    Ajout valider au forlulaire d'ajout de programme

--- src/Form/ProgrammeType.php
+++ src/Form/ProgrammeType.php
@@ -6,6 +6,7 @@ use App\Entity\Programme;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ProgrammeType extends AbstractType
 {
@@ -14,7 +15,7 @@ class ProgrammeType extends AbstractType
         $builder
             ->add('nb_jour')
             ->add('module')
-            ->add('session')
+            ->add('Valider', SubmitType::class)
         ;
     }
 
