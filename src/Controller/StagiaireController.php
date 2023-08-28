1ced01b84bcec994c7f97c092a26c19ecb95e2e9
Frederic Branca <frederic.branca@gmail.com>
2023-08-28 22:39:40 +0200

    Formatter

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -16,27 +16,31 @@ class StagiaireController extends AbstractController
     // Route pour ajouter ou éditer un stagiarie
     #[Route('/stagiaire/new', name: 'new_stagiaire')]
     #[Route('/stagiaire/{id}/edit', name: 'edit_stagiaire')]
-    public function new_edit(Stagiaire $stagiaire = null, Request $request, EntityManagerInterface $entityManager): Response
-    {
+    public function new_edit(
+        Stagiaire $stagiaire = null,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
         //Si stagiaire est null, alors on créé un objet de type Stagiaire
         $isNew = !$stagiaire;
         if ($isNew) {
             $stagiaire = new Stagiaire();
-        }   
+        }
 
         $form = $this->createForm(StagiaireType::class, $stagiaire);
 
         $form->handleRequest($request);
 
+        // Si le formulaire est valide
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $stagiaire = $form -> getData();
+            $stagiaire = $form->getData();
             // Prepare PDO
             $entityManager->persist($stagiaire);
             // execute PDO
             $entityManager->flush();
 
-            return $this->redirectToRoute('infos_stagiaire', ['id' => $stagiaire->getId()] );
+            return $this->redirectToRoute('infos_stagiaire', ['id' => $stagiaire->getId()]);
         }
 
         return $this->render('stagiaire/new.html.twig', [
@@ -47,7 +51,9 @@ class StagiaireController extends AbstractController
     }
 
     #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(StagiaireRepository $stagiaireRepository): Response
+    public function index(
+        StagiaireRepository $stagiaireRepository
+    ): Response
     {
         $stagiaires = $stagiaireRepository->findBy([], ['nom' => 'ASC']);
         return $this->render('stagiaire/index.html.twig', [
@@ -57,7 +63,9 @@ class StagiaireController extends AbstractController
 
     // Route pour afficher les infos d'un stagiaire
     #[Route('stagiaire/{id}', name: 'infos_stagiaire')]
-    public function infos(Stagiaire $stagiaire ): Response
+    public function infos(
+        Stagiaire $stagiaire
+    ): Response
     {
         return $this->render('stagiaire/infos.html.twig', [
             'stagiaire' => $stagiaire
@@ -66,7 +74,10 @@ class StagiaireController extends AbstractController
 
     // Route pour supprimer un stagiaire
     #[Route('/stagiaire/{id}/delete', name: 'delete_stagiaire')]
-    public function delete(Stagiaire $stagiaire = null, EntityManagerInterface $entityManager)
+    public function delete(
+        Stagiaire $stagiaire = null, 
+        EntityManagerInterface $entityManager
+    )
     {
         $entityManager->remove($stagiaire);
         $entityManager->flush();
