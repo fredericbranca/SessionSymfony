5c24a0dc4020e524f085055cc1923aa68b53a9ec
Frederic Branca <frederic.branca@gmail.com>
2023-08-29 09:08:33 +0200

    FormType pour ajouter une session

--- src/Form/SessionType.php
+++ src/Form/SessionType.php
@@ -7,19 +7,30 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class SessionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nb_place')
-            ->add('date_debut')
-            ->add('date_fin')
-            ->add('stagiaires')
-            ->add('formation')
-            ->add('Valider', SubmitType::class)
-        ;
+            ->add('formation', null, [
+                'required' => true
+            ])
+            ->add('date_debut', DateType::class, [
+                'widget' => 'single_text',
+                'input'  => 'datetime_immutable',
+                'required' => true
+            ])
+            ->add('date_fin', DateType::class, [
+                'widget' => 'single_text',
+                'input'  => 'datetime_immutable',
+                'required' => true
+            ])
+            ->add('nb_place', null, [
+                'required' => true
+            ])
+            ->add('Valider', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
