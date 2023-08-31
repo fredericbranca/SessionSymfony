afe04a3bc93b016f8e2b69fe7322336e512e89e9
Frederic Branca <frederic.branca@gmail.com>
2023-08-31 15:47:59 +0200

    Création d'un LogType (désactivé pour le moment)

---
+++ src/Form/LogType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class LogType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $lastUsername = $options['last_username'];
+
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'name' => 'email',
+                    'value' => $lastUsername
+                ] 
+            ])
+            ->add('password', PasswordType::class, [
+                'attr' => [
+                    'name' => 'password',
+                ]
+            ])
+            ->add('valider', SubmitType::class, [
+                'label' => 'Se connecter'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'last_username' => []
+        ]);
+    }
+}
