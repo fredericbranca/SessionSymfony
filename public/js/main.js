3492f87930fdee01b39f40a71f8d92c3e6e0deea
Frederic Branca <frederic.branca@gmail.com>
2023-09-05 00:22:44 +0200

    Réorganisation sur fichier main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,19 @@
-// NAVBAR - Fonction pour ouvrir ou fermer le bandeau de la navbar
-function toggleMenu() {
-  const hamburger_button = document.querySelector(".hamburger");
-  const nav_list = document.querySelector(".nav-links");
-  const nav_overlay = document.querySelector("#overlay");
-  const body = document.querySelector("body");
-  const content = document.querySelector(".content");
+// Déclaration des variables sans initialisation
+let hamburger_button, nav_list, nav_overlay, body, content, links;
+// Attend que le DOM soit chargé
+document.addEventListener("DOMContentLoaded", function () {
 
-  if (window.innerWidth < 992) {
-    if (!hamburger_button.classList.contains("is-active")) {
+  // Initialisation des variables
+  hamburger_button = document.querySelector(".hamburger");
+  nav_list = document.querySelector(".nav-links");
+  nav_overlay = document.querySelector("#overlay");
+  body = document.querySelector("body");
+  content = document.querySelector(".content");
+  links = document.querySelectorAll(".confirm-delete");
+
+  // Fonction pour ouvrir ou fermer le bandeau de la navbar
+  function toggleMenu(open = true) {
+    if (open) {
       hamburger_button.classList.add("is-active");
       nav_list.classList.add("is-active");
       nav_overlay.style.zIndex = 1;
@@ -31,37 +37,16 @@ function toggleMenu() {
       content.classList.add("content-out");
     }
   }
-}
 
-function closeMenu() {
-  const hamburger_button = document.querySelector(".hamburger");
-  const nav_list = document.querySelector(".nav-links");
-  const nav_overlay = document.querySelector("#overlay");
-  const body = document.querySelector("body");
-  const content = document.querySelector(".content");
+  // Ajoute un écouteur d'événement au bouton hamburger pour basculer le menu
+  hamburger_button.addEventListener("click", function () {
+    toggleMenu();
+  });
 
-  hamburger_button.classList.remove("is-active");
-  nav_list.classList.remove("is-active");
-  nav_overlay.style.zIndex = -1;
-  nav_overlay.style.display = "none";
-  body.style.backgroundColor = "var(--bg-color-1)";
-  content.style.zIndex = "0";
-  nav_list.classList.remove("nav-active");
-  content.classList.remove("content-active");
-}
-
-// Attend que le DOM soit chargé
-document.addEventListener("DOMContentLoaded", function () {
-  // Ajout d'une confirmation de supression sur les liens ayant la classe confirm-delete
-  // Récupère les liens avec la classe confirm-delete
-  const links = document.querySelectorAll(".confirm-delete");
-
-  // On vérifie qu'il existe au moins un lien
+  // Ajout d'une confirmation de suppression sur les liens ayant la classe confirm-delete
   if (links.length > 0) {
     links.forEach(function (link) {
-      // Ajout d'un écouteur d'évènement au clic d'un lien
       link.addEventListener("click", function (event) {
-        // confirm() Permet d'afficher une confirmation de suppression en annulant l'action par défaut du lien
         if (!confirm("Êtes-vous sûr de vouloir supprimer cet élément ?")) {
           event.preventDefault();
         }
@@ -69,25 +54,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  const hamburger_button = document.querySelector(".hamburger");
-  const nav_list = document.querySelector(".nav-links");
-
   // Écoute les clics
   document.addEventListener("click", function (event) {
-    console.log(event.target);
     // Si le menu est ouvert et que le clic n'est ni sur le bouton hamburger, ni dans le menu lui-même, alors fermez le menu
     if (hamburger_button.classList.contains("is-active")) {
       if (
-        !hamburger_button.contains(event.target) &&
-        !nav_list.contains(event.target)
+        !hamburger_button.contains(event.target)
       ) {
-        toggleMenu();
+        toggleMenu(false);
       }
     }
   });
 
-  window.addEventListener("resize", function () {
-    closeMenu();
-  });
+  window.addEventListener('resize', function() {
+    toggleMenu(false);
+});
 
 });
