2892641dff5857ae5ee0b8d285b52e3ddb05b104
Frederic Branca <frederic.branca@gmail.com>
2023-09-06 12:36:19 +0200

    gestion animations quand le bandeau (menu) s'ouvre

--- public/js/main.js
+++ public/js/main.js
@@ -9,27 +9,39 @@ document.addEventListener("DOMContentLoaded", function () {
   body = document.querySelector("body");
   content = document.querySelector(".content");
   links = document.querySelectorAll(".confirm-delete");
+  wrapper = document.querySelector('#wrapper');
 
   // Fonction pour ouvrir ou fermer le bandeau de la navbar
   function toggleMenu(open = true) {
     if (open) {
       hamburger_button.classList.add("is-active");
       nav_list.classList.add("is-active");
-      nav_list.style.animation = "slide-in 0.5s both;"
-      body.classList.add('overflow');
-      nav_overlay.style.zIndex = 1;
+      nav_list.style.animation = "slide-in 0.5s both";
+      content.style.transform = "translateX(90%)";
+      content.style.transition = "transform 0.5s";
       content.style.zIndex = "-1";
+      nav_overlay.style.zIndex = 1;
       nav_overlay.style.display = "block";
+      wrapper.classList.add('overflow');
+      body.style.overflow = 'hidden';
     } else {
       hamburger_button.classList.remove("is-active");
-      nav_list.classList.remove("is-active");
-      body.classList.remove('overflow');
-      nav_list.style.animation = "slide-out 0.5s both;"
+      wrapper.classList.remove('overflow');
+      nav_list.style.animation = "slide-out 0.5s both";
+      content.style.transform = "translateX(0%)";
       nav_overlay.style.display = "none";
       nav_overlay.style.zIndex = -1;
       content.style.zIndex = "0";
+      body.style.overflow = 'none';
     }
   }
+
+  // Permet de faire l'animation du content avant de le mettre en display none
+  nav_list.addEventListener('animationend', function (event) {
+    if (event.animationName === 'slide-out') {
+      nav_list.classList.remove("is-active");
+    }
+  });
 
   // Ajout d'une confirmation de suppression sur les liens ayant la classe confirm-delete
   if (links.length > 0) {
@@ -58,13 +70,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  window.addEventListener("resize", function() {
-    console.log(this.innerWidth);
+  // Enlève les styles d'animation du menu burger et du bandeau quand la largeur de la fenêtre est > 991px (mode bureau)
+  window.addEventListener("resize", function () {
+
     if (this.innerWidth > 991) {
       toggleMenu(false);
-      nav_list.classList.remove("nav-active");
-      nav_list.classList.remove("nav-out");
-    } 
-
+      content.style.transform = "none";
+      content.style.transition = "none";
+      nav_list.classList.remove("is-active");
+      nav_list.style.animation = "none";
+    }
   })
 });
